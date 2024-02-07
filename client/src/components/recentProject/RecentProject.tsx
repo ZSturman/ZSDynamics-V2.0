7496c562710e1e4e5470e82680f735ef7953298e
Frew Arega <zasturman@gmail.com>
2024-02-07 11:58:48 +1000

    Added recent projects section

--- client/src/components/recentProject/RecentProject.tsx
+++ client/src/components/recentProject/RecentProject.tsx
@@ -4,6 +4,7 @@ import { VideoComponent } from "./VideoComponent";
 import "./RecentProject.scss";
 
 const RecentProject = () => {
+  const videoUrl = "https://www.youtube.com/watch?v=pFvSaTfKbXI";
   return (
     <section id="recentProject">
       <div className="recent-project">
@@ -16,28 +17,39 @@ const RecentProject = () => {
 
         <div className="video-and-description">
           <div className="video-player">
-            <VideoComponent />
+            <VideoComponent videoUrl={videoUrl} />
           </div>
 
           <div className="description">
             <p>
-                There are a lot of applications out there to help with creating SDDs, but they are often too complex and require a lot of time to learn. This app is designed to be simple and easy to use.
+              There are a lot of applications out there to help with creating
+              SDDs, but they are often too complex and require a lot of time to
+              learn. This app is designed to be simple and easy to use.
             </p>
             <p>
-                Current features include:
-                <ul>
-                    <li>Creating a new document with chapters an sections</li>
-                    <li>Adding custom styling</li>
-                    <li>Automatic page breaks based on A4 paper dimensions</li>
-                    <li>Uploading images</li>
-                    <li>Customizable cover page</li>
-                    <li>Exporting a document to a PDF</li>
-                </ul>
+              Current features include:
+              <ul>
+                <li>Creating a new document with chapters an sections</li>
+                <li>Adding custom styling</li>
+                <li>Automatic page breaks based on A4 paper dimensions</li>
+                <li>Uploading images</li>
+                <li>Customizable cover page</li>
+                <li>Exporting a document to a PDF</li>
+              </ul>
             </p>
             <p>
-                Feel free to clone the repo and try it out for yourself. I am always looking for feedback and ways to improve the app.
+              Feel free to clone the repo and try it out for yourself. I am
+              always looking for feedback and ways to improve the app.
             </p>
-            <button className="repo-button"><a href="https://github.com/ZSturman/ReactApp-SoftwareDevelopmentDocument.git" target="_blank">SDD-Creator</a></button>
+    
+              <a
+                href="https://github.com/ZSturman/ReactApp-SoftwareDevelopmentDocument.git"
+                target="_blank"
+                className="btn-primary btn"
+              >
+                SDD-Creator
+              </a>
+
           </div>
         </div>
       </div>
--- client/src/components/recentProject/VideoComponent.tsx
+++ client/src/components/recentProject/VideoComponent.tsx
@@ -1,5 +1,17 @@
 import ReactPlayer from 'react-player'
 
-export const VideoComponent = () => (
-  <ReactPlayer url='https://www.youtube.com/watch?v=pFvSaTfKbXI' playing controls />
+interface VideoComponentProps {
+    videoUrl: string;
+}
+
+export const VideoComponent: React.FC<VideoComponentProps> = ({videoUrl}) => (
+    <ReactPlayer
+    className='react-player'
+    url={videoUrl}
+    playing
+    controls
+    width='100%'
+    height='100%'
+/>
+
 );
